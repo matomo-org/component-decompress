2b273e9f19a86d0e848a6258877646dd8125c283
sgiehl <stefan@piwik.org>
2016-01-03 20:58:09 +0100

    improved TAR tests

--- tests/TarTest.php
+++ tests/TarTest.php
@@ -20,7 +20,25 @@ class TarTest extends BaseTest
         $res = $unzip->extract($this->tempDirectory);
         $this->assertTrue($res);
 
+        $content = $unzip->listContent();
+
+        $this->assertCount(3, $content);
+        $this->assertEquals('tarout1.txt', $content[0]['filename']);
+        $this->assertEquals('tardir/tarout2.txt', $content[1]['filename']);
+        $this->assertEquals('tardir/', $content[2]['filename']);
+
         $this->assertFileContentsEquals('TESTDATA', $this->tempDirectory . 'tarout1.txt');
         $this->assertFileContentsEquals('MORETESTDATA', $this->tempDirectory . 'tardir/tarout2.txt');
+    }
+
+    public function testUnzipInvalidFile2()
+    {
+        $filename = $this->fixtureDirectory . '/NotExisting.zip';
+
+        $unzip = new Tar($filename);
+        $res = $unzip->extract($this->tempDirectory);
+        $this->assertEquals(0, $res);
+
+        $this->assertContains('Unable to open in read mode', $unzip->errorInfo());
     }
 }
